84788479a9c8a6b7dacc133be230d3ec59e83b9e
aries <aries.liuky@gmail.com>
2016-09-13 09:57:02 +0100

    helloworld

--- ULTimeTable/urls.py
+++ ULTimeTable/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from course import views as course_views
 
 urlpatterns = [
+    url(r'^course/', course_views.index),
     url(r'^admin/', admin.site.urls),
 ]
