167965af81008510627c78478b9985cb78c7c38b
aries <aries.liuky@gmail.com>
2016-09-13 16:24:55 +0100

    catch course content

---
+++ course/course.py
@@ -0,0 +1,82 @@
+#author: Hang Liu date:13/0917
+import urllib2, urllib
+import re
+from HTMLParser import HTMLParser
+
+class CourseParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.content = ''
+        self.is_content = False
+        self.col = 0
+        #index1=> which day eg.: Mon, Tue, Wed
+        #index2=> which course
+        self.courses = [[] for i in range(6)]
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'td':
+            for k, v in attrs:
+                if k == 'align' and v == 'justify':
+                    self.is_content = True
+        elif tag == 'hr' and self.is_content == True:
+            self.courses[self.col].append(self.content)
+            self.content = ''
+
+    def handle_endtag(self, tag):
+        if tag == 'td':
+            if self.is_content == True:
+                self.col += 1
+                self.is_content = False
+
+    def handle_data(self, data):
+        if self.is_content and len(data.strip())>0:
+            self.content += data.rstrip().lstrip()
+
+    def reset(self):
+        HTMLParser.reset(self)
+
+
+class Course():
+    def __init__(self, course_id):#LM039 LM634
+        print 'course course id:'+course_id
+        data = {'T1' : course_id, 'T2' : '1'}
+        post = urllib2.urlopen(
+            url = 'http://timetable.ul.ie/course_res.asp',
+            data = urllib.urlencode(data))
+
+        self.course_list = CourseParser()
+        self.course_list.feed(post.read())
+
+    def get_courses_list(self):
+        return self.course_list.courses
+
+    #data: eg.:12:00-13:00MA6011-LEC-MAYERHOFER EBERHARD MRSG19Wks:1-12
+    def get_detail(self, data, data_type):
+        pattern = re.compile(r'(\d{2}:\d{2})-(\d{2}:\d{2})([A-Z]+\d+)-([A-Z]+)-(\d+[A-Z]){0,}(.*?)((((B)|(SG)|(S)|(KBG)|(KB)|(CSG)|(CS)|(GLG)|(GL)|(FB)|(FG)|(F)|(ERB)|(ER)|(ER)|(LCB)|(LC)|(LCO)|(LB)|(LG)|(L)|(SR)|(PG)|(PM)|(P)|(HSG)|(HS)|(AM)|(A)|(BM)|(CG)|(CM)|(C)|(DG)|(DM)|(D)|(EG)|(EM)|(E)|(IWG)|(IW)){1}(M|O){0,1}\d{2,}\s{0,})+\w{0,})Wks:(.*)')        #re.compile(r'(\d{2}:\d{2})-(\d{2}:\d{2})([A-Z]+\d+)-([A-Z]+)-(\d+[A-Z]){0,}(.*?)(((B|SG|S|KBG|KB|CSG|CS|GLG|GL|FB|FG|F|ERB|ER|ER|LCB|LC|LCO|LB|LG|L|SR|PG|PM|P|HSG|HS|AM|A|BM|CG|CM|C|DG|DM|D|EG|EM|E|IWG|IW|M|O)+\d+\s{0,})+\w{0,})Wks:(.*)')
+        match = pattern.match(data)
+        if match:
+            if data_type == 'START_TIME':
+                return match.group(1)
+            elif data_type == 'END_TIME':
+                return match.group(2)
+            elif data_type == 'COURSE_ID':
+                return match.group(3)
+            elif data_type == 'COURSE_TYPE':
+                return match.group(4)
+            elif data_type == 'GROUP':
+                return match.group(5)
+            elif data_type == 'TEACHER':
+                return match.group(6)
+            elif data_type == 'CLASS_ROOM':
+                return match.group(7)
+            elif data_type == 'WEEKS':
+                return match.group(52)
+            else:
+                return
+
+#CourseParser.get_time(course_list.courses[2][2])
+#CourseParser.get_module_id(course_list.courses[2][2])
+#print CourseParser.get_detail(course_list.courses[2][1],'END_WEEK')
+
+
+
--- course/views.py
+++ course/views.py
@@ -1,7 +1,109 @@
+#@author: Hang Liu date:13/09/17
 from django.shortcuts import render
 from django.http import HttpResponse
+import time
+from course import Course, CourseParser
+from datetime import datetime, timedelta
+########################################
+#var
+########################################
 
-# Create your views here.
+# Date & Time
+#first_day = input('Enter your the first day of week 1:')
+today = datetime.today()
+strtoday = today.strftime("%Y%m%d")
+day1_of_week1 = datetime.strptime('05/09/2016', '%d/%m/%Y')
+course_id = "ULTimeTable"
+course = type(Course)
+subcontents = ''
+
+########################################
+#function
+########################################
 def index(request):
-    course_id = request.GET['id']
-    return HttpResponse('Module ID:'+str(course_id))
+    global course_id,course
+    course_id = str(request.GET['id'])
+    course = Course(course_id)
+    file = open(course_id+".ics","w")
+    events()
+    content = '''BEGIN:VCALENDAR
+METHOD:PUBLISH
+VERSION:2.0
+X-WR-CALNAME:'''+course_id+'''
+PRODID:-//Apple Inc.//Mac OS X 10.11.4//EN
+CALSCALE:GREGORIAN
+X-WR-TIMEZONE:Europe/Dublin
+'''+subcontents+'''
+END:VCALENDAR
+'''
+    file.write(content)
+    return HttpResponse('Course ID:'+course_id)
+
+def week_range(wks):
+    temp = wks.split(',')
+    result = []
+    inner = []
+    for i in temp:
+        a = i.split('-')
+        if len(a) == 2:
+            inner = [k for k in range(int(a[0]),int(a[1])+1)]
+        else:
+            result.append(a[0])
+        for j in inner:
+            result.append(j)
+        inner = []
+    return result
+
+def day_event(start_time, end_time, course_id, course_type, group, teacher, class_room, weeks, event_date, week_index):
+    global today
+    start_time_str = datetime.strftime(datetime.strptime(start_time, '%H:%M'), '%H%M%S')
+    end_time_str = datetime.strftime(datetime.strptime(end_time, '%H:%M'), '%H%M%S')
+    event_date_str = event_date.strftime("%Y%m%d")
+    today_str = today.strftime("%Y%m%d")
+    subcontent = '''
+BEGIN:VEVENT
+DTSTART:'''+event_date_str+'T'+start_time_str+'''
+DTEND:'''+event_date_str+'T'+end_time_str+'''
+DTSTAMP:'''+event_date_str+'''T'''+start_time_str+'''
+UID:-'''+event_date_str+start_time_str+end_time_str+class_room+'''
+CLASS:PUBLIC
+CREATED:'''+event_date_str+'''T000000Z
+DESCRIPTION:'''+teacher+':'+class_room+':Wks:'+weeks+'''
+LAST-MODIFIED:'''+today_str+'''T074945Z
+LOCATION:Limerick
+SEQUENCE:1
+STATUS:CONFIRMED
+SUMMARY:'''+'W'+str(week_index)+' '+class_room+' '+course_id+'-'+course_type+'-'+group+'''
+TRANSP:TRANSPARENT
+END:VEVENT'''
+    return subcontent
+
+#@index: which day, Mon=>0, Tue=>1, until Saturday=>5
+def day_events(index):
+    global subcontents,course
+    courses = course.get_courses_list()
+    for e in courses[index]:
+        start_time = course.get_detail(e, 'START_TIME')
+        end_time = course.get_detail(e, 'END_TIME')
+        course_id = course.get_detail(e, 'COURSE_ID')
+        course_type = course.get_detail(e, 'COURSE_TYPE')
+        group = course.get_detail(e, 'GROUP')
+        if group == None:
+            group = ''
+        teacher = course.get_detail(e, 'TEACHER')
+        class_room = course.get_detail(e, 'CLASS_ROOM')
+        weeks = course.get_detail(e, 'WEEKS')
+        week_ranges = week_range(weeks)
+        start_week_index = int(week_ranges[0]) - 1
+        end_week_index = int(week_ranges[len(week_ranges)-1])
+        day = day1_of_week1 + timedelta(days=index)
+        for i in range(0, end_week_index):
+            if i+1 in week_ranges:
+                subcontents += day_event(start_time, end_time, course_id, course_type, group, teacher, class_room, weeks, day, i+1)
+            day += timedelta(days=7)
+
+
+def events():
+    for index in range(6):
+        day_events(index)
+
